1ca9ea184c98c5a402c00ff582beca28464c5af6
haiderrkhann <139079559+haiderrkhann@users.noreply.github.com>
2023-07-10 06:45:36 -0400

    Add files via upload

---
+++ 5_PalindromeDetector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string word = Console.ReadLine();
+            string[] wordArray = word.Split(' ');
+            string reverse = string.Empty;
+            for (int i=0; i< wordArray.Length; i++)
+            {
+                for(int j= wordArray.Length-1-i;j>0;j--)
+                {
+                    if(wordArray[i] == wordArray[j])
+                    {
+                    
+                    }
+                    
+                }
+                Console.WriteLine("Palindrome(s): " + wordArray[i]);
+            }
+                Console.ReadKey();
+        }
+    }
+}
