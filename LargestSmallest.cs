f353f118479bdcd52195ba52566700916c8ba177
haiderrkhann <139079559+haiderrkhann@users.noreply.github.com>
2023-07-10 08:08:54 +0500

    Add files via upload

---
+++ LargestSmallest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textfrequency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordarray = Console.ReadLine();
+            string[] word = wordarray.Split(' ');
+            
+            int min = word[0].Length;
+            int max = word[0].Length;
+            string largest = word[0];
+            string smallest = word[0];
+            for (int i = 1; i <= word.Length-1; i++)
+            {
+                int length = word[i].Length;
+                if (length > max)
+                {
+                    largest = word[i];
+                    max = length;
+                }
+                if (length < min)
+                {
+                    smallest = word[i];
+                    min = length;
+                    
+                }
+            }
+            Console.WriteLine("Longest:   " + largest);
+            Console.WriteLine("Shortest:   " + smallest);
+            Console.ReadKey();
+        }
+    }
+}
