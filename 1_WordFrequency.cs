2312ad28b8ba329a2af387c32f0e9961ebe5afdb
haiderrkhann <139079559+haiderrkhann@users.noreply.github.com>
2023-07-10 16:59:00 -0400

    Add files via upload

---
+++ 1_WordFrequency.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a string:");
+        string s = Console.ReadLine();
+        string[] words = s.Split(' ');
+        Console.WriteLine("\n");
+        Console.WriteLine("Word frequencies:");
+        for (int i = 0; i < words.Length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < i; j++)
+            {
+                if (words[i] == words[j])
+                {
+                    count++;
+                    break;
+                }
+            }
+
+            if (count > 0)
+                continue;
+            for (int j = i; j < words.Length; j++)
+            {
+                if (words[i] == words[j])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"{words[i]}: {count}");
+            Console.ReadKey();
+        }
+    }
+}
